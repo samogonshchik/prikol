fb67e3abb0c9bed73d8a9b68537080b0e8a243e7
andrew <rabykin2005@gmail.com>
2025-05-14 03:21:57 +0300

    moved reference source files, cleaned up a bit

--- app/src/old/java/com/example/prikol/MainActivity.kt
+++ app/src/old/java/com/example/prikol/MainActivity.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
+import androidx.navigation.compose.rememberNavController
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import com.example.prikol.navigation.PrikolNavHost
 import com.example.prikol.ui.theme.PrikolTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+//                    PrikolNavHost(rememberNavController())
                     TestPython()
                 }
             }
